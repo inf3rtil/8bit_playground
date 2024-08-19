328f3fdf5d1a77d68fdea6c373cf840b85dd5dc3
Fernando Ferreira Silva <fernando@fer.dev.br>
2024-08-19 18:33:50 -0300

    asm blink

---
+++ avr/blink_asm/main.asm
@@ -0,0 +1,28 @@
+	;; LED blink atmega328p - SENAI 
+	.include "m328Pdef.inc"
+	
+	.cseg
+	.org 0
+	ldi r20, 0x00
+	ldi r21, 0xFF
+	jmp main
+	
+main:
+	cli
+	ldi r24, 100
+	out PORTB, r20
+	out DDRB, r21
+	call delay
+	out PORTB, r21
+	ldi r24, 100
+	call delay
+	jmp main
+	
+delay:
+	ldi   r31, 255
+loop:
+	sbiw    r30, 1
+	brne    loop
+	sbiw 	  r24, 1
+	brne    delay
+	ret
